89de418ff131d074c372fd48a5ed5e6e6db4cbf2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-06-03 11:01:22 +0200

    Add wallet class, change included names

---
+++ wallet.js
@@ -0,0 +1,52 @@
+class Wallet {
+
+    constructor(money) {
+
+        let _money = money;
+
+        //pobieranie aktualnej zawartści portfela
+
+        this.getWalletValue = () => _money;
+
+        //sprawdzaine czy użytkownik ma odpowwiednią ilość środków do gry
+
+        this.checkCanPlay = value => {
+
+            if(_money >= value) return true;
+            return false;
+
+        }
+
+        this.changeWallet = (value, type = "+") => {
+
+            if(typeof value === "number" && !isNaN(value)){
+
+                if (type === "+"){
+
+                    return _money += value;
+
+                } else if (type === "-"){
+
+                    return _money -= value;
+
+                } else {
+
+                    throw new Error("nieprawidłowy typ działania");
+
+                }
+
+            } else {
+
+                console.log(typeof value);
+                throw new Error ("nieprawidłowa liczba");
+
+            }
+
+        }
+
+    }
+
+}
+
+
+const wallet = new Wallet(200)
