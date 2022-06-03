38ac4b44c7ec988206929cc7ab98a76ba6eebae0
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-06-03 11:36:40 +0200

    Add statistics clasS

---
+++ statistics.js
@@ -0,0 +1,36 @@
+class Statistics {
+
+    constructor() {
+
+        this.gameResults = [{win: true, bid: 2}, {win: false, bid: -10}, {win: false, bid: -10}];
+
+    }
+
+    addGameToStatistics(win, bid){
+
+        //dodawanie wyniku gry do tablicy
+        let gameResult = {
+
+            win: win, 
+            bid: bid
+
+        }
+
+        console.log(gameResult);
+        this.gameResults.push(gameResult)
+    }
+
+    showGameStatistics() {
+
+        //sprawdzaanie ilości gier
+        let games = this.gameResults.length;
+        //sprawdzanie ilości wygranych
+        let wins = this.gameResults.filter(result => result.win).length;
+        //sprawdzanie ilości przegranych
+        let losses = this.gameResults.filter(result =>!result.win).length;
+        console.log(games, wins, losses);
+        // zwraca ilośc gier, wygranych i przegranych
+        return [games, wins, losses];
+    }
+
+}
